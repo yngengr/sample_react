a742aa1bc7599edc655376cb99f02db035064281
princox <rjhoon92@naver.com>
2021-09-08 20:36:10 +0900

    first commit

---
+++ precache-manifest.432255177e5fb35ce00659901f2b5c77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18309db46d93b20c716701ff0aa4fa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cd300a6fc99cbe5d895",
+    "url": "/static/css/main.5a032b4e.chunk.css"
+  },
+  {
+    "revision": "deacecf959496c1df5e7",
+    "url": "/static/js/2.45a5db28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.45a5db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd300a6fc99cbe5d895",
+    "url": "/static/js/main.41aab495.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  }
+]);
